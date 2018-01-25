859781f398f3507c7bcb7d749959ff1b4087fe1f
roy-van-dijk <roy.van.dijk@outlook.com>
2018-01-25 14:02:17 +0100

    Schonere code + comments + icons

--- level_4.py
+++ level_4.py
@@ -1,41 +1,58 @@
 import time 
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 import threading
 
+#Geen Waarschuwingen en juiste schema voor pins
 GPIO.setmode(GPIO.BCM) 
 GPIO.setwarnings(False)
 
+#GPIO pins motoren
 motor1 = [2,3,4,17]
 motor2 = [27,22,10,9]
 
+#GPIO PINS sensoten en knoppen
 sensorl = 14
 sensorm = 15
 sensorr = 18
-
 knop = 26
 knopl = 6
 knopm = 19
 knopr = 13
-
 lichtRA = 5
 lichtRV = 12
 lichtLA = 25
 lichtLV = 24
 
-##Seq voor en achter uit
+#Lichtrichting en richting begin waardes
+lichtrichting = "vooruit"
+richting = "vooruit"
+
+#Standaardwaarde lichtknipperen
+lichtknipper = True
+
+##Sequenses
 SeqVoorAchter1 = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
 SeqVoorAchter2 = [[0,0,0,1],[0,0,1,1],[0,0,1,0],[0,1,1,0],[0,1,0,0],[1,1,0,0],[1,0,0,0],[1,0,0,1]]
 
+#Lengte van sequens en controle of beide even groot zijn
+stepcountVoorAchter1 = len(SeqVoorAchter1)
+stepcountVoorAchter2 = len(SeqVoorAchter2)
+stepcountvoorAchter = 0
+if (stepcountVoorAchter1 == stepcountVoorAchter2):
+    stepcountVoorAchter = stepcountVoorAchter1
+
+#Vertragingsvariabele voor motors
 delay = 2/float(1000)
 
+#pinnen van motoren op outputs zetten en op 0
 for pin in motor1:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
-
 for pin in motor2:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
-    
+
+#GPIO pinnen op inputs zetten   
 GPIO.setup(sensorl, GPIO.IN)
 GPIO.setup(sensorm, GPIO.IN)
 GPIO.setup(sensorr, GPIO.IN)
@@ -44,24 +61,40 @@ GPIO.setup(knopl, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(knopr, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(knopm, GPIO.IN, GPIO.PUD_UP)
 
+#GPIO pinnen op uitputs zetten
 GPIO.setup(lichtRA,GPIO.OUT)
 GPIO.setup(lichtRV,GPIO.OUT)
 GPIO.setup(lichtLA,GPIO.OUT)
 GPIO.setup(lichtLV,GPIO.OUT)
 
-GPIO.output(lichtRA, GPIO.LOW)
-GPIO.output(lichtRV, GPIO.LOW)
-GPIO.output(lichtLA, GPIO.LOW)
-GPIO.output(lichtLV, GPIO.LOW)
-    
-stepcountVoorAchter1 = len(SeqVoorAchter1)
-stepcountVoorAchter2 = len(SeqVoorAchter2)
+#Juiste lichtjes aan
+def lichtjesuit () :
+    GPIO.output(lichtRA, GPIO.LOW)
+    GPIO.output(lichtRV, GPIO.LOW)
+    GPIO.output(lichtLA, GPIO.LOW)
+    GPIO.output(lichtLV, GPIO.LOW)
 
-stepcountvoorAchter = 0
+def lichtjesLinks() :
+    GPIO.output(lichtRA, GPIO.LOW)
+    GPIO.output(lichtRV, GPIO.LOW)
+    GPIO.output(lichtLA, GPIO.HIGH)
+    GPIO.output(lichtLV, GPIO.HIGH)
 
-if (stepcountVoorAchter1 == stepcountVoorAchter2):
-    stepcountVoorAchter = stepcountVoorAchter1
-    
+def lichtjesAan() :
+    GPIO.output(lichtRA, GPIO.HIGH)
+    GPIO.output(lichtRV, GPIO.HIGH)
+    GPIO.output(lichtLA, GPIO.HIGH)
+    GPIO.output(lichtLV, GPIO.HIGH)
+
+def lichtjesRechts() :
+    GPIO.output(lichtRA, GPIO.HIGH)
+    GPIO.output(lichtRV, GPIO.HIGH)
+    GPIO.output(lichtLA, GPIO.LOW)
+    GPIO.output(lichtLV, GPIO.LOW)
+
+#Lichtjes uitzetten
+lichtjesuit()
+
 def rij(motor, seq) :
     GPIO.output(motor[0], seq[0])
     GPIO.output(motor[1], seq[1])
@@ -71,28 +104,31 @@ def rij(motor, seq) :
 def vooruit(step) :
     global motor1
     global motor2
+    global lichtrichting
+    #Beide motortjes een andere richting draaien
     rij(motor1, SeqVoorAchter1[step])
     rij(motor2, SeqVoorAchter2[step])
-
-def achteruit(step) :
-    global motor1
-    global motor2
-    rij(motor1, SeqVoorAchter2[step])
-    rij(motor2, SeqVoorAchter1[step])
+    lichtrichting = "vooruit"
     
 def rechts(step):
     global motor1
     global motor2
+    global lichtrichting
+    #Beide motortjes dezelfde richting draaien
     rij(motor1, SeqVoorAchter1[step])
     rij(motor2, SeqVoorAchter1[step])
+    lichtrichting = "rechts"
 
 def links(step):
     global motor1
     global motor2
+    global lichtrichting
+    #Beide motortjes dezelfde richting draaien
     rij(motor2, SeqVoorAchter2[step])
     rij(motor1, SeqVoorAchter2[step])
+    lichtrichting = "links"
 
-def rondjewiel(richting, hoeveelheid):
+def draaiwiel(richting, hoeveelheid):
     global delay
     global stepcountVoorAchter
     aantal = hoeveelheid * 256
@@ -104,37 +140,40 @@ def rondjewiel(richting, hoeveelheid):
             rechts(step % stepcountVoorAchter)
         if (richting == "vooruit") :
             vooruit(step % stepcountVoorAchter)
-        
 
-kruispunt = False
-
-step = 0
-
-richting = "vooruit"
-
-def start():
-    global step
-    global kruispunt
+#Rijden van auto
+def rijden():
+    step = 0
+    kruispuntGehad = False
     while True:
+        #Kruispunt gedetecteerd
         if (not GPIO.input(sensorl) and not GPIO.input(sensorm) and not GPIO.input(sensorr)) :
-            if (kruispunt) :
+            #Controleren op tweede keer kruispunt
+            if (kruispuntGehad) :
+                #Tweede keer kruispunt dus stoppen
                 break
             else :
                 if (richting == "links") :
-                    rondjewiel("vooruit", 3)
-                    rondjewiel("links", 8)
-                    rondjewiel("vooruit", 2)
+                    #Handmatige bocht op kruising
+                    draaiwiel("vooruit", 3)
+                    draaiwiel("links", 8)
+                    draaiwiel("vooruit", 2)
                 if (richting == "vooruit") :
-                    rondjewiel("vooruit", 2)
+                    #Handmatige bocht op kruising
+                    draaiwiel("vooruit", 2)
                 if (richting == "rechts") :
-                    rondjewiel("vooruit", 3)
-                    rondjewiel("rechts", 8)
-                    rondjewiel("vooruit", 2)
-                kruispunt = True
+                    #Handmatige bocht op kruising
+                    draaiwiel("vooruit", 3)
+                    draaiwiel("rechts", 8)
+                    draaiwiel("vooruit", 2)
+                kruispuntGehad = True
+        #Naar links gedetecteerd
         elif (not GPIO.input(sensorl) and GPIO.input(sensorr)) :
             links(step)
+        #Naar recht gedetecteerd
         elif (GPIO.input(sensorl) and not GPIO.input(sensorr)) :
             rechts(step)
+        #Niks gedetecteerd
         else  :
             vooruit(step)
         if(step == stepcountVoorAchter - 1):
@@ -143,59 +182,49 @@ def start():
                 step = step + 1
         time.sleep(delay)
 
-knipperstart = 0
-lichtknipper = True
-GPIO.output(lichtRA, GPIO.HIGH)
-GPIO.output(lichtRV, GPIO.HIGH)
-GPIO.output(lichtLA, GPIO.HIGH)
-GPIO.output(lichtLV, GPIO.HIGH)
-
-while True:
-    if (not GPIO.input(knop)) :
-        kruispunt = False
-        start()
-    if (not GPIO.input(knopr)) :
-        richting = "rechts"
-        GPIO.output(lichtRA, GPIO.HIGH)
-        GPIO.output(lichtRV, GPIO.HIGH)
-        GPIO.output(lichtLA, GPIO.LOW)
-        GPIO.output(lichtLV, GPIO.LOW)
-    if (not GPIO.input(knopm)) :
-        richting = "vooruit"
-        GPIO.output(lichtRA, GPIO.HIGH)
-        GPIO.output(lichtRV, GPIO.HIGH)
-        GPIO.output(lichtLA, GPIO.HIGH)
-        GPIO.output(lichtLV, GPIO.HIGH)
-    if (not GPIO.input(knopl)) : 
-        richting = "links"
-        GPIO.output(lichtRA, GPIO.LOW)
-        GPIO.output(lichtRV, GPIO.LOW)
-        GPIO.output(lichtLA, GPIO.HIGH)
-        GPIO.output(lichtLV, GPIO.HIGH)
-    knipperstart = knipperstart + 1
-    if (knipperstart > 10) :
-        knipperstart = 0
-        if (lichtknipper == True) :
+#Licht laten knipperen
+def knipper() :
+    global lichtrichting
+    global lichtknipper
+    while True:
+        if (lichtknipper):
             lichtknipper = False
-            GPIO.output(lichtRA, GPIO.LOW)
-            GPIO.output(lichtRV, GPIO.LOW)
-            GPIO.output(lichtLA, GPIO.LOW)
-            GPIO.output(lichtLV, GPIO.LOW)
+            if (lichtrichting == "links"):
+                lichtjesLinks()
+            if (lichtrichting == "rechts"):
+                lichtjesRechts()
+            if (lichtrichting == "vooruit"):
+                lichtjesAan()
         else:
             lichtknipper = True
-            if (richting == "rechts") :
-                GPIO.output(lichtRA, GPIO.HIGH)
-                GPIO.output(lichtRV, GPIO.HIGH)
-                GPIO.output(lichtLA, GPIO.LOW)
-                GPIO.output(lichtLV, GPIO.LOW)
-            if (richting == "vooruit") :
-                GPIO.output(lichtRA, GPIO.HIGH)
-                GPIO.output(lichtRV, GPIO.HIGH)
-                GPIO.output(lichtLA, GPIO.HIGH)
-                GPIO.output(lichtLV, GPIO.HIGH)
-            if (richting == "links") : 
-                GPIO.output(lichtRA, GPIO.LOW)
-                GPIO.output(lichtRV, GPIO.LOW)
-                GPIO.output(lichtLA, GPIO.HIGH)
-                GPIO.output(lichtLV, GPIO.HIGH)
+            lichtjesuit()
+        time.sleep(1)
+
+#Lichtknipper code apart laten draaien
+lichtenknipperen = threading.Thread(target=knipper)
+lichtenknipperen.start()
+
+#Start code
+while True:
+    #Knop starten ingedrukt
+    if (not GPIO.input(knop)) :
+        kruispunt = False
+        rijden()
+        richting = "vooruit"
+    #Knop rechts ingedrukt
+    elif (not GPIO.input(knopr)) :
+        richting = "rechts"
+        lichtenknipperen = True
+        lichtjesRechts()
+    #Knop vooruit ingedrukt
+    elif (not GPIO.input(knopm)) :
+        richting = "vooruit"
+        lichtenknipperen = True
+        lichtjesAan()
+    #Knop links ingedrukt
+    elif (not GPIO.input(knopl)) : 
+        richting = "links"
+        lichtenknipperen = True
+        lichtjesLinks()
+    lichtrichting = richting
     time.sleep(0.1)
